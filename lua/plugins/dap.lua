3c2e0a2725a2c9da6ee0ae28fdea342c3199aabe
OPthyago <thyagoop@gmail.com>
2025-03-14 06:18:58 -0300

    changing the dap keymaps

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -12,51 +12,123 @@ return {
   },
   keys = {
     {
-      vim.keymap.set("n", "<leader>da", function()
-        vim.notify("Starting debug session...", vim.log.levels.INFO)
-        require("dap").continue()
-      end, { desc = "Start Debugging" }),
+      "<leader>dB",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end,
+      desc = "Breakpoint Condition",
     },
     {
-      vim.keymap.set("n", "<leader>du", function()
-        require("dapui").toggle()
-      end, { desc = "Toggle Debug UI" }),
-    },
-    {
-      vim.keymap.set("n", "<leader>dr", function()
-        require("dap").repl.toggle()
-      end, { desc = "Toggle Debug REPL" }),
-    },
-    {
-      vim.keymap.set("n", "<leader>dl", function()
-        local log_file = vim.fn.stdpath("data") .. "/dap.log"
-        vim.cmd("edit " .. log_file)
-      end, { desc = "Open DAP Log" }),
-    },
-    {
-      vim.keymap.set("n", "<leader>dc", require("dap").continue, { desc = "Continue" }),
-    },
-    {
-      vim.keymap.set("n", "<leader>do", require("dap").step_over, { desc = "Step Over" }),
-    },
-    {
-
-      vim.keymap.set("n", "<leader>di", require("dap").step_into, { desc = "Step Into" }),
-    },
-    {
-      vim.keymap.set("n", "<leader>dlp", function()
-        require("dap").set_breakpoint(nil, nil, vim.fn.input("Log message: "))
-      end, { desc = "Logpoint" }),
-    },
-    {
-      vim.keymap.set("n", "<leader>db", function()
+      "<leader>db",
+      function()
         require("dap").toggle_breakpoint()
-      end, { desc = "Toggle Breakpoint" }),
+      end,
+      desc = "Toggle Breakpoint",
     },
     {
-      vim.keymap.set("n", "<leader>dB", function()
-        require("dap").set_breakpoint(vim.fn.input("Condition: "))
-      end, { desc = "Conditional Breakpoint" }),
+      "<leader>dc",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Run/Continue",
+    },
+    {
+      "<leader>da",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Start debugging",
+    },
+    {
+      "<leader>dC",
+      function()
+        require("dap").run_to_cursor()
+      end,
+      desc = "Run to Cursor",
+    },
+    {
+      "<leader>dg",
+      function()
+        require("dap").goto_()
+      end,
+      desc = "Go to Line (No Execute)",
+    },
+    {
+      "<leader>di",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Step Into",
+    },
+    {
+      "<leader>dj",
+      function()
+        require("dap").down()
+      end,
+      desc = "Down",
+    },
+    {
+      "<leader>dk",
+      function()
+        require("dap").up()
+      end,
+      desc = "Up",
+    },
+    {
+      "<leader>dl",
+      function()
+        require("dap").run_last()
+      end,
+      desc = "Run Last",
+    },
+    {
+      "<leader>do",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Step Out",
+    },
+    {
+      "<leader>dO",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Step Over",
+    },
+    {
+      "<leader>dP",
+      function()
+        require("dap").pause()
+      end,
+      desc = "Pause",
+    },
+    {
+      "<leader>dr",
+      function()
+        require("dap").repl.toggle()
+      end,
+      desc = "Toggle REPL",
+    },
+    {
+      "<leader>ds",
+      function()
+        require("dap").session()
+      end,
+      desc = "Session",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Terminate",
+    },
+    {
+      "<leader>dw",
+      function()
+        require("dap.ui.widgets").hover()
+      end,
+      desc = "Widgets",
     },
   },
   config = function()
@@ -158,5 +230,7 @@ return {
     dap.listeners.before.event_exited["dapui_config"] = function()
       dapui.close()
     end
+
+    -- Keymaps
   end,
 }
