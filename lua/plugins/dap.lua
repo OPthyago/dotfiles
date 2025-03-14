58b48da191e6344b181dcf18beb71e4b16b1b0a6
OPthyago <thyagoop@gmail.com>
2025-03-14 01:02:44 -0300

    fixing the debug plugin to work with serverless
    removing unnused files and adding new keymaps
    
    commenting the log line

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,158 +1,149 @@
-local js_based_languages = {
-  "typescript",
-  "javascript",
-  "typescriptreact",
-  "javascriptreact",
-  "vue",
-}
-
 return {
-  { "nvim-neotest/nvim-nio" },
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-      "theHamsta/nvim-dap-virtual-text",
-      {
-        "microsoft/vscode-js-debug",
-        build = "npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && mv dist out",
-        version = "1.*",
-      },
-      {
-        "mxsdev/nvim-dap-vscode-js",
-        config = function()
-          require("dap-vscode-js").setup({
-            debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
-            adapters = {
-              { type = "pwa-node", port = 9223 },
-              { type = "pwa-chrome", port = 9222 },
-            },
-          })
-        end,
-      },
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    {
+      "williamboman/mason.nvim",
+      opts = function(_, opts)
+        opts.ensure_installed = opts.ensure_installed or {}
+        table.insert(opts.ensure_installed, "js-debug-adapter")
+      end,
     },
-    config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      local Config = require("lazyvim.config")
+  },
+  config = function()
+    local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local Config = require("lazyvim.config")
+    local vscode = require("dap.ext.vscode")
 
-      dap.set_log_level("DEBUG")
-      dapui.setup()
+    --Setting the log level
+    --dap.set_log_level("TRACE") -- uncomment this line to enable comments
 
-      -- Configuração de ícones
-      vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-      for name, sign in pairs(Config.icons.dap) do
-        sign = type(sign) == "table" and sign or { sign }
-        vim.fn.sign_define("Dap" .. name, {
-          text = sign[1],
-          texthl = sign[2] or "DiagnosticInfo",
-          linehl = sign[3],
-          numhl = sign[3],
-        })
+    --UI Setting
+    vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
+    for name, sign in pairs(Config.icons.dap) do
+      sign = type(sign) == "table" and sign or { sign }
+      vim.fn.sign_define("Dap" .. name, {
+        text = sign[1],
+        texthl = sign[2] or "DiagnosticInfo",
+      })
+    end
+
+    dapui.setup({
+      layouts = {
+        {
+          elements = { "scopes", "breakpoints", "stacks", "watches" },
+          size = 40,
+          position = "left",
+        },
+        {
+          elements = { "repl", "console" },
+          size = 10,
+          position = "bottom",
+        },
+      },
+    })
+
+    --Setting adapters
+    if not dap.adapters["pwa-node"] then
+      require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            LazyVim.get_pkg_path("js-debug-adapter", "/js-debug/src/dapDebugServer.js"),
+            "${port}",
+          },
+        },
+      }
+    end
+
+    if not dap.adapters["node"] then
+      dap.adapters["node"] = function(cb, config)
+        if config.type == "node" then
+          config.type = "pwa-node"
+        end
+        local nativeAdapter = dap.adapters["pwa-node"]
+        if type(nativeAdapter) == "function" then
+          nativeAdapter(cb, config)
+        else
+          cb(nativeAdapter)
+        end
       end
+    end
 
-      -- Configurações para linguagens baseadas em JS
-      for _, language in ipairs(js_based_languages) do
+    vscode.type_to_filetypes["node"] = js_filetypes
+    vscode.type_to_filetypes["pwa-node"] = js_filetypes
+
+    for _, language in ipairs(js_filetypes) do
+      if not dap.configurations[language] then
         dap.configurations[language] = {
           {
             type = "pwa-node",
             request = "launch",
-            name = "Debug Current Test",
-            runtimeExecutable = "node",
-            runtimeArgs = {
-              "--experimental-vm-modules",
-              "--no-warnings",
-              "${workspaceFolder}/node_modules/.bin/jest",
-              "${file}",
-              "--runInBand",
-              "--watch",
-            },
+            name = "Launch file",
+            program = "${file}",
             cwd = "${workspaceFolder}",
-            console = "integratedTerminal",
-            internalConsoleOptions = "neverOpen",
-            skipFiles = { "<node_internals>/**" },
-            sourceMaps = true,
-            env = {
-              NODE_OPTIONS = "--experimental-vm-modules --no-warnings",
-              TS_JEST_DISABLE_VER_CHECKER = "true",
-            },
-            resolveSourceMapLocations = {
-              "${workspaceFolder}/**",
-              "!**/node_modules/**",
-            },
           },
           {
             type = "pwa-node",
-            request = "launch",
-            name = "Run All Tests",
-            runtimeExecutable = "node",
-            runtimeArgs = {
-              "--experimental-vm-modules",
-              "--no-warnings",
-              "${workspaceFolder}/node_modules/.bin/jest",
-              "--runInBand",
-              "--watchAll",
-            },
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
             cwd = "${workspaceFolder}",
-            console = "integratedTerminal",
-            internalConsoleOptions = "neverOpen",
-            skipFiles = { "<node_internals>/**" },
-            sourceMaps = true,
-            env = {
-              NODE_OPTIONS = "--experimental-vm-modules --no-warnings",
-              TS_JEST_DISABLE_VER_CHECKER = "true",
-            },
-          },
-          {
-            type = "pwa-chrome",
-            request = "launch",
-            name = "Launch Chrome",
-            url = "http://localhost:3000",
-            webRoot = "${workspaceFolder}",
-            userDataDir = "${workspaceFolder}/.vscode/chrome",
-            sourceMaps = true,
           },
         }
       end
+    end
 
-      -- Carregar launch.json
-      if vim.fn.filereadable(".vscode/launch.json") then
-        require("dap.ext.vscode").load_launchjs(nil, {
-          ["pwa-node"] = js_based_languages,
-          ["pva-chrome"] = js_based_languages,
-        })
-      end
+    -- Listeners to open and close dapui
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open({ reset = true })
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
 
-      -- Keymaps
-      vim.keymap.set("n", "<leader>da", function()
-        vim.notify("Iniciando sessão de debug...", vim.log.levels.INFO)
-        dap.continue()
-      end, { desc = "Start Debugging" })
+    -- Keymaps
+    vim.keymap.set("n", "<leader>da", function()
+      vim.notify("Starting debug session...", vim.log.levels.INFO)
+      dap.continue()
+    end, { desc = "Start Debugging" })
 
-      vim.keymap.set("n", "<leader>du", function()
-        dapui.toggle()
-      end, { desc = "Toggle Debug UI" })
+    vim.keymap.set("n", "<leader>du", function()
+      dapui.toggle()
+    end, { desc = "Toggle Debug UI" })
 
-      vim.keymap.set("n", "<leader>dr", function()
-        dap.repl.toggle()
-      end, { desc = "Toggle Debug REPL" })
+    vim.keymap.set("n", "<leader>dr", function()
+      dap.repl.toggle()
+    end, { desc = "Toggle Debug REPL" })
 
-      vim.keymap.set("n", "<leader>dl", function()
-        local log_file = vim.fn.stdpath("data") .. "/dap.log"
-        vim.cmd("edit " .. log_file)
-      end, { desc = "Open DAP Log" })
-      -- Breakpoints
-      vim.keymap.set("n", "<leader>db", function()
-        require("dap").toggle_breakpoint()
-      end, { desc = "Toggle Breakpoint" })
+    vim.keymap.set("n", "<leader>dl", function()
+      local log_file = vim.fn.stdpath("data") .. "/dap.log"
+      vim.cmd("edit " .. log_file)
+    end, { desc = "Open DAP Log" })
 
-      vim.keymap.set("n", "<leader>dB", function()
-        require("dap").set_breakpoint(vim.fn.input("Condition: "))
-      end, { desc = "Conditional Breakpoint" })
+    vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Continue" })
+    vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Step Over" })
+    vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step Into" })
 
-      vim.keymap.set("n", "<leader>dlp", function()
-        require("dap").set_breakpoint(nil, nil, vim.fn.input("Log message: "))
-      end, { desc = "Logpoint" })
-    end,
-  },
+    vim.keymap.set("n", "<leader>dlp", function()
+      require("dap").set_breakpoint(nil, nil, vim.fn.input("Log message: "))
+    end, { desc = "Logpoint" })
+
+    -- Breakpoints
+    vim.keymap.set("n", "<leader>db", function()
+      require("dap").toggle_breakpoint()
+    end, { desc = "Toggle Breakpoint" })
+
+    vim.keymap.set("n", "<leader>dB", function()
+      require("dap").set_breakpoint(vim.fn.input("Condition: "))
+    end, { desc = "Conditional Breakpoint" })
+  end,
 }
